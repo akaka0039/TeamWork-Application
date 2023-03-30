fbdbf98af66cd0a6b5bbcce2e3259598f76f96ce
Chris Telford <christelford@hotmail.co.uk>
2023-03-29 18:22:35 -0700

    Task schema/model updated; consider seperating logic

--- server/models/Task.js
+++ server/models/Task.js
@@ -6,21 +6,39 @@ const TaskSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    // or ObjectId?
     memberId: {
-      type: Array,
+      type: String,
+      required: true,
     },
     status: {
       type: String,
-      required: true,
+      // what are the statuses?
+      default: 1,
+      // // required: true, // can probably use a default value
     },
+    // can probably use a default value
     priority: {
       type: String,
-      required: true,
+      default: "medium",
+      // // required: true, // can probably use a default value
     },
-  },
+  }
 
-  { timestamps: true }
+  // is this necessary?
+  // { timestamps: true }
 );
 
+// when converting from bson to json, format id and remove version
+// toJson runs when using, eg. res.json() or JSON.stringify()
+TaskSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Task = mongoose.model("Task", TaskSchema);
+
 export default Task;
